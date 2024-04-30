be05bf8ae921364df739c2c5ea833770ff0d0d80
wint-phyusin-maung <sshonn687@gmail.com>
2024-04-30 13:57:18 +0630

    install: WyW-in-js

--- frontend/src/component/home.js
+++ frontend/src/component/home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { css } from '@linaria/core';
+
+const h1 = css`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
 
 export default function home() {
     return (
         <div>
-            <h1>Welcome to our home </h1>
+            <h1 className={h1}>Welcome to our home </h1>
         </div>
     )
 }
+
