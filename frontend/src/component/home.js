63c8e3d1fef755834b3179e4ccd1a1b3c8c96224
wint-phyusin-maung <sshonn687@gmail.com>
2024-04-30 21:36:08 +0630

    add: navigation

--- frontend/src/component/home.js
+++ frontend/src/component/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { css } from '@linaria/core';
+import { css } from '@emotion/css'
 
 const h1 = css`
   font-size: 24px;
   font-weight: bold;
+  color: orange;
 `;
 
 
--- frontend/src/component/navigation.js
+++ frontend/src/component/navigation.js
@@ -1,14 +1,52 @@
 import React from 'react'
+import { css } from '@emotion/css'
 import { Link } from 'react-router-dom'
+import CIcon from '@coreui/icons-react';
+import { cilShieldAlt } from '@coreui/icons';
+
+
+const Nav = css`
+   width: 80%;
+   height:8vh;
+   margin: 0 auto;
+   display: flex;
+   justify-content: space-between;
+   border-bottom: 1px solid #ccc;
+`
+
+const Navbar = css`
+    display: flex;
+
+    li {
+        list-style-type: none;
+        padding: 1.2rem;
+    }
+`
+
+const span = css`
+    font-weight: bold;
+`
 
 export default function Navigation() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-            </ul>
+        <nav className={Nav}>
+            <div>
+                <h6>
+                    <CIcon icon={cilShieldAlt} size="md" style={{ '--ci-primary-color': 'blue' }} />
+                    <span className={span}>KopoKie</span>
+                </h6>
+
+            </div>
+            <div>
+                <ul className={Navbar}>
+                    <li>
+                        <Link style={{ textDecoration: 'none' }} to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link style={{ textDecoration: 'none' }} to='/logout'>Logout</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
