620e3c82d8e67699a0a6d0751197ce0f7659878f
wint-phyusin <gnext.wintphyusinmaung@gmail.com>
2024-05-15 23:20:38 +0630

    add: change color

--- frontend/src/component/home.js
+++ frontend/src/component/home.js
@@ -4,6 +4,7 @@ import { css } from '@emotion/css';
 const h1 = css`
   font-size: 24px;
   font-weight: bold;
+  color: white;
 `;
 
 const divOne = css`
@@ -15,11 +16,42 @@ const divOne = css`
   background-repeat: no-repeat;
 `;
 
+const subOne = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  background: green;
+  backdrop-filter: blur(10px);
+  width: 50%;
+  align-items: flex-start;
+  padding: 3rem;
+  margin-right: 28rem;
+
+  h1,p {
+    padding: 1.2rem;
+  }
+`
+
+const Button = css`
+  border: none;
+  padding: 1rem .5rem;
+  border-radius: .5rem;
+  font-weight: bold;
+
+`
+
 export default function Home() {
     return (
         <div className={divOne}>
-            <h1 className={h1}>Welcome to our home</h1>
-            <p></p>
+          <div className={subOne}>
+            <h1 className={h1}>Your Gateway to Exquisite Living</h1>
+            <p>Welcome to Discover Elegance, where sophistication and convenience intertwine. Dive into a world of refined luxury,
+               where every aspect of your lifestyle is elevated with meticulously curated products and experiences.
+            </p>
+            <button className={Button}>
+              Free Charging places for this month
+            </button>
+          </div>
         </div>
     );
 }
